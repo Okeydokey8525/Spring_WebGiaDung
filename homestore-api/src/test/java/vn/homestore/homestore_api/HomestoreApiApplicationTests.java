81f8b01b4b98a7810b5242b6f2d9187e640b492b
Le Duc Luong <leducluong.08052005@gmail.com>
2026-08-09 12:23:09 +0700

    chore: initialize Spring Boot backend

---
+++ homestore-api/src/test/java/vn/homestore/homestore_api/HomestoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.homestore.homestore_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomestoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
